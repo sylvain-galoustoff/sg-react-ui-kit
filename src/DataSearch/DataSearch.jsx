126add420eb8afcd590e37304774c78845e41f0d
Sylvain-MED <146923279+Sylvain-MED@users.noreply.github.com>
2024-06-24 15:20:41 +0200

    Controlled input ok

--- src/DataSearch/DataSearch.jsx
+++ src/DataSearch/DataSearch.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import style from "./DataSearch.module.scss";
 
-const DataSearch = ({ data }) => {
+const DataSearch = ({ data, containerClass, inputClass, addonClass, placeholder }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <div>
-      <input type="text" />
-      <div className="addon">
+    <div className={`${style.container} ${containerClass}`}>
+      <input
+        type="text"
+        className={`${style.input} ${inputClass}`}
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <div className={`${style.addon} ${addonClass}`}>
         <IoSearch />
       </div>
     </div>
