302a9df7c9daf077c476d6b08dc7570f882f0323
Sylvain-MED <-v>
2024-07-01 13:11:44 +0200

    DataTable : Add sort button to columns

--- src/DataTable/ColumnName.jsx
+++ src/DataTable/ColumnName.jsx
@@ -1,7 +1,16 @@
+import { IoCaretDown, IoCaretUp } from "react-icons/io5";
 import style from "./DataTable.module.scss";
 
 function ColumnName({ label }) {
-  return <th className={`${style.th} columnName`}>{label}</th>;
+  return (
+    <th className={`${style.th} columnName`}>
+      <span className={style.sortButtons}>
+        <IoCaretUp />
+        <IoCaretDown />
+      </span>
+      <span className={style.columnLabel}>{label}</span>
+    </th>
+  );
 }
 
 export default ColumnName;
